84f6d9b7b4d783fe0e559cbc5b0c021b5c9dea1f
Nelson <njpinget@gmail.com>
2023-03-26 11:56:51 -0300

    feat: Agregar servicio de autenticación fix: Corregir error de autenticación refactor: Simplificar código de autenticación

--- tema6-Routing/my-app/src/app/app.module.ts
+++ tema6-Routing/my-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -8,6 +8,7 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { ContactsPageComponent } from './pages/contacts-page/contacts-page.component';
 import { ContactDetailPageComponent } from './pages/contact-detail-page/contact-detail-page.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,9 @@ import { ContactDetailPageComponent } from './pages/contact-detail-page/contact-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- tema6-Routing/my-app/src/app/pages/login-page/login-page.component.ts
+++ tema6-Routing/my-app/src/app/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +8,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-  constructor(private router:Router){}
+  
+  email:string='';
+  password:string='';
+
+
+  constructor(private router:Router, private authService:AuthService){}
+
   ngOnInit(): void {
+
     let token = sessionStorage.getItem('token');
+
     if(token){
       this.router.navigate(['home']);
     }
@@ -17,7 +26,19 @@ export class LoginPageComponent implements OnInit {
   }
   
   loginUser(){
-    sessionStorage.setItem('token', '123456789');
-    this.router.navigate(['contacts']);
+
+    this.authService.login(this.email, this.password).subscribe(
+      (response)=>{
+        if(response.token){
+          sessionStorage.setItem('token', response.token);
+          this.router.navigate(['home']);
+        }
+      },
+      (error) => console.error(`ha ocurrido un error en el login: ${error}`),
+      () =>  console.info('proceso de login terminado.')
+      
+    )
+
+    
   }
 }
